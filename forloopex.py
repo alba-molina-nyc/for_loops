4cfe79ab2864badaacde8b384e486e592f2ab67e
Username <albavmolinanyc@gmail.com>
2022-06-16 16:33:22 -0400

    i modules 2 == 0 for loop

--- forloopex.py
+++ forloopex.py
@@ -1,8 +1,5 @@
 """
-create a loop that logs the numbers from -99 (ascending)"""
-
-from pydoc import describe
-
+1.create a loop that logs the numbers from -99 (ascending)"""
 
 nums = range(0, 100, 1)
 
@@ -11,13 +8,11 @@ for n in nums:
     break
 
 """
-create a loop that logs the numbers from 99-0 (descending)"""
-
+2.create a loop that logs the numbers from 99-0 (descending)"""
 
 for n in nums:
     # print(sorted(nums, reverse=True))
     break
-
 
 """
 try with less code: 
@@ -28,3 +23,21 @@ for i in range(0,100,1):
      print(sorted(range(0,100,1), reverse=True), 'descending')
      break
 
+"""3. Create a loop that logs the EVEN numbers from 0-98 (ascending)"""
+
+for i in range(0,100,1):
+    if i % 2 == 0:
+        print(i, 'evens')
+  
+
+"""4. Create a loop that logs the EVEN numbers from 98-0 (descending)"""
+
+"""5. Create a loop that logs the ODD numbers from 0-99 (ascending)"""
+
+"""6. Create a loop that logs the ODD numbers from 99-0 (descending)"""
+
+"""7. Create a loop that logs the numbers from 49-72 (ascending)"""
+
+"""8. Create a loop that logs the numbers from 81-26 (descending)"""
+
+"""9. Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)"""
