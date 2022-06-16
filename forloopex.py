58debe24a85ce905f26bd494a844f0ddf1797576
Username <albavmolinanyc@gmail.com>
2022-06-16 15:47:35 -0400

    second ex

--- forloopex.py
+++ forloopex.py
@@ -4,4 +4,14 @@ create a loop that logs the numbers from -99 (ascending)"""
 nums = range(0, 100, 1)
 
 for n in nums:
-    print(n)
+    print(sorted(nums))
+    break
+
+"""
+create a loop that logs the numbers from 99-0 (descending)"""
+
+
+for n in nums:
+    print(sorted(nums, reverse=True))
+    break
+    
